4440ecf91e7ac73532e537afc1dd1a5ff60a989a
Роман <rezample@gmail.com>
2022-08-03 18:20:24 +0400

    -

--- src/main/java/com/example/springrest/resolver/ResolverConfig.java
+++ src/main/java/com/example/springrest/resolver/ResolverConfig.java
@@ -1,14 +1,11 @@
-package com.example.springrest.config;
+package com.example.springrest.resolver;
 
 import com.example.springrest.resolver.UserHandlerMethodArgumentResolver;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
-@Configuration
+
 public class ResolverConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
